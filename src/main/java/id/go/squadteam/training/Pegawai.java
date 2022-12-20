58009a83393b457e2c703ecc91a81b662b10c8d6
Ahmad Muzaki Munir <ahmadmuzakimunir@gmail.com>
2022-12-20 16:15:39 +0700

    Function

---
+++ src/main/java/id/go/squadteam/training/Pegawai.java
@@ -0,0 +1,20 @@
+package id.go.squadteam.training;
+
+public class Pegawai {
+    private String nama;
+    private String nip;
+    private String email;
+    private String telepon;
+
+    public void pengajuanCuti() {
+    }
+
+    public void persetujuanCuti() {
+    }
+
+    public void penolakanCuti() {
+    }
+
+    public void pembatalanCuti() {
+    }
+}
